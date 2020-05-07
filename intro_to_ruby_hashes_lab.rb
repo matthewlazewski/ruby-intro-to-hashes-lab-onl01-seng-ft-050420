2ecfac19b71f5cef443652bc71ff8de76650033f
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-07 18:03:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,9 @@ def monopoly_with_third_tier
         :three_pieces_owned => 100, 
         :four_pieces_owned => 200
       },
-      :names => {}
+      :names => {
+        :reading_railraod => {}
+      }
     } 
    }
 end

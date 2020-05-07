83a1ade16dd20b942a5c45a3f09316cfd2367f9b
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-07 18:07:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ def monopoly_with_fourth_tier
         },
         :names => {
           :reading_railroad => {
-            "mortage _value" => "$100"
+            "mortage_value" => "$100"
           },
           :pennsylvania_railroad => {},
           :b_and_o_railroad => {},
